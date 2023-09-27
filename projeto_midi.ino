4a1a9547a04b5b1848567674da220f44f6d2af80
Levi Ribeiro <126264441+ribeiroLevi@users.noreply.github.com>
2023-09-27 19:18:53 -0300

    :ok_hand: style: alteração nos comentários

--- projeto_midi.ino
+++ projeto_midi.ino
@@ -1,42 +1,27 @@
 #include "MIDIUSB.h"
 
 const byte TOTAL_BUTTONS = 5;
-// All the Arduino pins used for buttons, in order.
 const byte BUTTONS_PIN[TOTAL_BUTTONS] = {2,3,4,5,6};
-// Every pitch corresponding to every Arduino pin. Each note has an associated numeric pitch (frequency scale).
-// See https://github.com/arduino/tutorials/blob/master/ArduinoZeroMidi/PitchToNote.h
+//https://github.com/arduino/tutorials/blob/master/ArduinoZeroMidi/PitchToNote.h
 const byte BUTTONS_PITCH[TOTAL_BUTTONS] = {60,62,64,65,67};
-// Current state of the pressed buttons.
 byte currentRead[TOTAL_BUTTONS];
-// Temporary input reads to check against current state.
 byte tempRead;
 
-// The setup function runs once when you press reset or power the board
 void setup() {
-  // Initialize all the pins as a pull-up input.
   for (byte i = 0; i < TOTAL_BUTTONS; i++) {
     pinMode(BUTTONS_PIN[i], INPUT_PULLUP);
   }
 }
 
-// The loop function runs over and over again forever
 void loop() {
   for (byte i = 0; i < TOTAL_BUTTONS; i++) {
-    // Get the digital state from the button pin.
-    // In pull-up inputs the button logic is inverted (HIGH is not pressed, LOW is pressed).
     byte buttonState = digitalRead(BUTTONS_PIN[i]);
-    // Temporarily store the digital state.
     tempRead = buttonState;
-    // Continue only if the last state is different to the current state.
     if (currentRead[i] != tempRead) {
-      // See https://www.arduino.cc/en/pmwiki.php?n=Tutorial/Debounce
       delay(2);
-      // Get the pitch mapped to the pressed button.
       byte pitch = BUTTONS_PITCH[i];
-      // Save the new input state.
       currentRead[i] = tempRead;
-      // Execute note on or noted off depending on the button state.
-      if (buttonState == LOW) {
+      if (buttonState == HIGH) {
         noteOn(pitch);
       } else {
         noteOff(pitch);
